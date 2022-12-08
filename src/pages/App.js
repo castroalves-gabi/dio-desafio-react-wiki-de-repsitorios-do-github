ee977ba07571fd6b8fd25a6bc70879e51146abad
castroalves-gabi <ile.bgk@gmail.com>
2022-12-08 13:53:38 -0300

    first commit

--- src/pages/App.js
+++ src/pages/App.js
@@ -34,9 +34,7 @@ function App() {
   }
 
   const handleRemoveRepo = (id) => {
-    console.log('Removendo registro', id);
-
-    // utilizar filter.
+    setRepos(repos.filter((repo) => repo.id !== id));
   }
 
 
